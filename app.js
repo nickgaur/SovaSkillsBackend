a72a25b6b4a06357647528d3c5acd5d00a61efcf
nickgaur <67472332+nickgaur@users.noreply.github.com>
2022-03-14 02:53:48 +0530

    sessions added

--- app.js
+++ app.js
@@ -1,32 +1,77 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
-const mongoose = require("mongoose");
 const dbUrl = "mongodb://localhost:27017/sova-skills";
+const User = require("./models/users.js");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
 
-//  CONNECTING THE MONGO DB DATABASE
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-});
+const password = "12345";
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected!!");
-});
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(dbUrl);
+  console.log("Database connected!!");
+}
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 // app.set("views", path.join(__dirname, "views"));
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname + "/index.html"));
-// });
+// app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "views")));
 
-// app.get("/login", (req, res) => {
-//   res.render("login");
-// });
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  }))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views", "index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, '/views', 'hello.html'));
+});
+
+// app.post('/login', (req, res) => {
+//
+// })
+
+app.get("/login/admin", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "/public/", "index.html"));
+  res.send("this is the login page for admin");
+});
+
+app.post("/login/admin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (validPassword) {
+      req.session.user_id = user._id;
+      res.redirect("/courses");
+    } else {
+      res.redirect("/");
+    }
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    res.redirect('/login')
+})
+app.get("/courses", (req, res) => {
+    if(!req.session.user_id){
+        res.redirect('/login')
+    }
+    res.sendFile(path.join(__dirname, '/views', 'logout.html'));
+});
+
 app.listen(3000, () => {
   console.log("APP IS STARTING!!");
 });
--- models/users.js
+++ models/users.js
@@ -1,15 +1,31 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const passportLocalMongoose = require("passport-local-mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
+// DEFINING SCHEMA
 const UserSchema = new Schema({
   email: {
     type: String,
-    require: true,
+    required: [true, "Email is required"],
     unique: true,
+    role: ['student', 'admin'],
+  },
+  password: {
+    type: String,
+    required: true,
   },
 });
 
-UserSchema.plugin(passportLocalMongoose);
+const User = mongoose.model("User", UserSchema);
+
+// CREATING ADMIN USER
+// const saltRounds = 10;
+// const admin_password = "admin@123";
+// const adminUser = new User({
+//   email: "admin@admin.com",
+//   password: bcrypt.hashSync(admin_password, saltRounds),
+// });
+
+// adminUser.save();
 
 module.exports = mongoose.model("User", UserSchema);
