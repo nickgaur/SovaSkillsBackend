5a7c352422ff4f8313182645deebb1d683e1b845
nickgaur <nikhilgaur3@rocketmail.com>
2022-03-18 15:28:29 +0530

    changed

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const adminLoginRoutes = require('./routes/adminLogin')
 const adminRoutes = require('./routes/admin')
 const cons = require('consolidate');
 const homeRoutes = require('./routes/home')
-const port = process.env.PORT || 8000
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/sova-skills";
+const port = process.env.PORT || 8000
 
 main().catch((err) => console.log(err));
 
